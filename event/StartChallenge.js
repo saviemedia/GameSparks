ed664485239516248abc0921b2cfcf6a1c9e5d65
Pierre-Olivier Dionne <podionne@gmail.com>
2017-02-08 20:50:43 -0500

    Add StartChallenge event

---
+++ event/StartChallenge.js
@@ -0,0 +1,9 @@
+// ====================================================================================================
+//
+// Cloud Code for StartChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+var idChallenge = Spark.getData().idChallenge;
+Spark.getChallenge(idChallenge).startChallenge();
