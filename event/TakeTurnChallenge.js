39d6c565a5c8ac811222db6eee1f2b4af0fc6de4
Pierre-Olivier Dionne <podionne@gmail.com>
2017-02-11 16:28:52 -0500

    Add TakeTurnChallenge event

---
+++ event/TakeTurnChallenge.js
@@ -0,0 +1,16 @@
+// ====================================================================================================
+//
+// Cloud Code for TakeTurnChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+
+var challengeId = Spark.getData().challengeId;
+var playerId = Spark.getData().playerId;
+var cellIndex = Spark.getData().cellIndex;
+
+var challenge = Spark.getChallenge(challengeId);
+challenge.takeTurn(playerId);
+
+Spark.setScriptData("cellIndex", cellIndex);
