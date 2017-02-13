ac462587e3ad00a211cd47eeee27374c2c589e39
Pierre-Olivier Dionne <podionne@gmail.com>
2017-02-13 13:13:35 -0500

    Rename TakeTurnChallenge for ConsumeTurnChallenge

---
+++ event/ConsumeTurnChallenge.js
@@ -0,0 +1,30 @@
+// ====================================================================================================
+//
+// Cloud Code for ConsumeTurnChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+
+var challengeId = Spark.getData().challengeId;
+var cellIndex = Spark.getData().cellIndex;
+var userId = Spark.getData().userId;
+
+var challenge = Spark.getChallenge(challengeId).consumeTurn(userId);
+
+var acceptedPlayerIds = challenge.getAcceptedPlayerIds();
+var arr_playerNotified = new Array();
+
+for (var i = 0; i < acceptedPlayerIds.length; i++) 
+{
+    if(acceptedPlayerIds[i] != userId)
+	{
+		arr_playerNotified.push(acceptedPlayerIds[i]);
+	}
+}
+
+var nsg = Spark.message("CHALLENGE_TURN_TAKEN");
+documentToSend = {"cellIndex" : cellIndex};
+nsg.setMessageData(documentToSend);
+nsg.setPlayerIds(arr_playerNotified);
+nsg.send();
