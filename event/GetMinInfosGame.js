95fe6e83086bc96e9c672f191ad8b7e23be96512
Pierre-Olivier Dionne <podionne@gmail.com>
2017-02-21 16:53:28 -0500

    Add matterJeu in GetMinInfosGame

--- event/GetMinInfosGame.js
+++ event/GetMinInfosGame.js
@@ -24,6 +24,7 @@ if(myConnectedPlayers.hasNext())
 
 Spark.setScriptData("runState", myChallenge.getRunState());
 Spark.setScriptData("idJeu", myChallenge.getScriptData("idJeu"));
+Spark.setScriptData("matterJeu", myChallenge.getScriptData("matterJeu"));
 Spark.setScriptData("maxPlayers", myChallenge.getScriptData("maxPlayers"));
 Spark.setScriptData("challengeSeed", myChallenge.getScriptData("challengeSeed"));
 Spark.setScriptData("challenger", myChallenger);
