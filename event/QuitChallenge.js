22cb81709506ca26460f8e9faa1e52032b50295d
Pierre-Olivier Dionne <podionne@gmail.com>
2017-02-28 13:47:35 -0500

    Add QuitChallenge

---
+++ event/QuitChallenge.js
@@ -0,0 +1,28 @@
+// ====================================================================================================
+//
+// Cloud Code for QuitChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+var challengeId = Spark.getData().challengeId;
+var myId = Spark.getPlayer().getPlayerId();
+
+var challenge = Spark.getChallenge(challengeId);
+
+var acceptedPlayerIds = challenge.getAcceptedPlayerIds();
+var arr_playerNotified = new Array();
+
+for (var i = 0; i < acceptedPlayerIds.length; i++) 
+{
+    if(acceptedPlayerIds[i] != myId)
+	{
+		arr_playerNotified.push(acceptedPlayerIds[i]);
+	}
+}
+
+var nsg = Spark.message("CHALLENGE_SURRENDERED");
+documentToSend = {};
+nsg.setMessageData(documentToSend);
+nsg.setPlayerIds(arr_playerNotified);
+nsg.send();
