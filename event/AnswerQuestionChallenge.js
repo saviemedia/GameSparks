f89841061554a008cc0572599e80e986b360310b
Pierre-Olivier Dionne <podionne@gmail.com>
2017-03-11 20:22:50 -0500

    Add steps during a turn

---
+++ event/AnswerQuestionChallenge.js
@@ -0,0 +1,30 @@
+// ====================================================================================================
+//
+// Cloud Code for AnswerQuestionChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+
+var challengeId = Spark.getData().challengeId;
+var userId = Spark.getData().userId;
+var noQuestion = Spark.getData().noQuestion;
+var isAnswerGood = Spark.getData().isAnswerGood;
+
+var challenge = Spark.getChallenge(challengeId);
+var acceptedPlayerIds = challenge.getAcceptedPlayerIds();
+var arr_playerNotified = new Array();
+
+for (var i = 0; i < acceptedPlayerIds.length; i++) 
+{
+    if(acceptedPlayerIds[i] != userId)
+	{
+		arr_playerNotified.push(acceptedPlayerIds[i]);
+	}
+}
+
+var nsg = Spark.message("CHALLENGE_QUESTION_ANSWERED");
+documentToSend = {"noQuestion" : noQuestion, "isAnswerGood" : isAnswerGood};
+nsg.setMessageData(documentToSend);
+nsg.setPlayerIds(arr_playerNotified);
+nsg.send();
---
+++ event/ChooseCellChallenge.js
@@ -0,0 +1,33 @@
+// ====================================================================================================
+//
+// Cloud Code for ChooseCellChallenge, write your code here to customise the GameSparks platform.
+//
+// For details of the GameSparks Cloud Code API see https://portal.gamesparks.net/docs.htm			
+//
+// ====================================================================================================
+
+var challengeId = Spark.getData().challengeId;
+var userId = Spark.getData().userId;
+var cellIndex = Spark.getData().cellIndex;
+var symbol = Spark.getData().symbol;
+var noQuestion = Spark.getData().noQuestion;
+var difficultyQuestion = Spark.getData().difficultyQuestion;
+
+
+var challenge = Spark.getChallenge(challengeId);
+var acceptedPlayerIds = challenge.getAcceptedPlayerIds();
+var arr_playerNotified = new Array();
+
+for (var i = 0; i < acceptedPlayerIds.length; i++) 
+{
+    if(acceptedPlayerIds[i] != userId)
+	{
+		arr_playerNotified.push(acceptedPlayerIds[i]);
+	}
+}
+
+var nsg = Spark.message("CHALLENGE_CELL_CHOSEN");
+documentToSend = {"cellIndex" : cellIndex, "symbol" : symbol, "noQuestion" : noQuestion, "difficultyQuestion" : difficultyQuestion};
+nsg.setMessageData(documentToSend);
+nsg.setPlayerIds(arr_playerNotified);
+nsg.send();
